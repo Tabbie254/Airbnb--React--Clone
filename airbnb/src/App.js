b91caa2bd25176d29b514082887373a265362fad
Tabitha Mutiso <tabbie440@gmail.com>
2021-01-21 17:41:06 +0300

    first Airbnb -clone - commit

---
+++ airbnb/src/App.js
@@ -0,0 +1,37 @@
+
+import './App.css'   
+import Home from './Home'
+import Header from './Header'
+
+function App() {
+  return (
+    //BEM 
+    <div className="app">
+      <Header/>
+      <Home/>
+
+
+
+{/**Home */}
+    {/* Header*/}
+
+    {/* Banner*/}
+        {/*Search*/}
+
+    {/* Cards*/}
+     
+    {/* Cards*/}
+  
+{/* searchPage*/}
+    {/* Header*/}
+
+    {/* ....*/}
+
+    
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ airbnb/src/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {Avatar} from "@material-ui/core"
+
+function Header() {
+    return (
+        <div className="header">
+       <img 
+        className="header__icon"
+         src="https://www.seekpng.com/png/detail/234-2340842_airbnb-logo.png" alt="Airbnb Logo@seekpng.com"/>
+         
+         <div className="header__center">
+            <input type="text"/>
+            <SearchIcon/>
+            
+         </div>
+
+         <div className="header__right">
+            <p>Become a host</p>
+            <LanguageIcon/>
+            <ExpandMoreIcon/>
+            <Avatar/>
+            </div>
+        </div>
+    )
+}
+
+export default Header
